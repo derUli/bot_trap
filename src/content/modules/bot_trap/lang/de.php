e37556d2af75659c876cc055e2a6be5361d42f16
Ulrich Schmidt <support@ulicms.de>
2018-08-13 16:18:31 +0200

    Handbuch

--- src/content/modules/bot_trap/lang/de.php
+++ src/content/modules/bot_trap/lang/de.php
@@ -1,5 +1,5 @@
 <?php
 add_translation("add_this_to_robots_txt", "Fügen Sie bitte folgende Zeile zu Ihrer robots.txt hinzu:");
-add_translation("trapped_bots", "Trapped Bots");
-add_translation("bot_trap", "Bot Trap");
-add_translation("bot_trap_custom_message", "Custom Message");
+add_translation("trapped_bots", "Blockierte Bots");
+add_translation("bot_trap", "Bot Falle");
+add_translation("bot_trap_custom_message", "Eigene Nachricht");
